41f8d9e25a3430a6e4e4c9f1a622d55efab1c762
sohamsudrik <sohamsudrik100@gmail.com>
2025-12-16 19:17:52 +0530

    Add files via upload

---
+++ program16.java
@@ -0,0 +1,19 @@
+
+public class program16
+{
+      public static void main(String[]args)
+      {
+           int total = 0;
+           int marks;
+
+           for(int i=1; i<=5; i++)
+           {
+            marks = 60;
+            total = total+marks;
+           } 
+
+           System.out.println("total mark:"+total);
+
+
+      }
+}
